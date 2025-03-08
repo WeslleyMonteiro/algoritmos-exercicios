6dca98c576909cd755f0d31f28b910def72abb7f
Weslley Monteiro <weslley.vs.monteiro@gmail.com>
2025-03-08 19:46:37 -0300

    ll

---
+++ ex010/script.js
@@ -0,0 +1,15 @@
+function areaTinta() {
+    const InputAltura = document.getElementById("InputAltura")
+    const altura = Number (InputAltura.value)
+
+    const inputLargura = document.getElementById("inputLargura")
+    const largura = Number (inputLargura.value)
+
+    const area = altura * largura
+    const tinta = area/2
+    document.getElementById("area").textContent = `A área da parede é ${area}m²`
+    document.getElementById("tinta").textContent = `O total de tinta utilizada é ${tinta} litros`
+
+    InputAltura.value = ""
+    inputLargura.value = ""
+}
