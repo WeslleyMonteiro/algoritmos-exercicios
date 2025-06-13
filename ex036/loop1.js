c65656ec2c50375c443a312d19b1431340683f02
Weslley Monteiro <weslley.vs.monteiro@gmail.com>
2025-06-12 23:16:13 -0300

    1010

---
+++ ex036/loop1.js
@@ -0,0 +1,6 @@
+let contador = 6
+
+while (contador <= 11) {
+    console.log(contador)
+    contador++
+}
---
+++ ex037/loop2.js
@@ -0,0 +1,6 @@
+let contador = 10
+
+while (contador >= 3){
+    console.log(contador)
+    contador--
+}
